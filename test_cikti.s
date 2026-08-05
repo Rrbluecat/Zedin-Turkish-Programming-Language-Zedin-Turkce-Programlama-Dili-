67f4ac59e9e3c81b0704621a553bc6b4edc2860f
Rrbluecat <pelitabdulkadir58@tutamail.com>
2026-08-05 13:43:22 +0300

    Faz 4: stack-tabanli yerel degisken + recursive fonksiyon native ARM64

--- test_cikti.s
+++ test_cikti.s
@@ -3,36 +3,81 @@
 main:
     stp x29, x30, [sp, #-16]!
     mov x29, sp
-    adr x0, _str_1
-    adr x1, _var_isim
-    str x0, [x1]
-    mov x0, #4
-    adr x1, _var_versiyon
-    str x0, [x1]
-    adr x19, _str_2
-    adr x1, _var_isim
-    ldr x20, [x1]
-    adr x0, _fmt_str_str
+    mov x0, #5
+    mov x19, x0
+    mov x0, x19
+    stp x22, x23, [sp, #-16]!
+    bl kare
+    ldp x22, x23, [sp], #16
+    mov x19, x0
+    adr x0, _fmt_sayi
     mov x1, x19
-    mov x2, x20
     bl printf
-    adr x19, _str_3
-    adr x1, _var_versiyon
-    ldr x0, [x1]
-    mov x20, x0
-    adr x0, _fmt_str_sayi
+    mov x0, #6
+    mov x19, x0
+    mov x0, x19
+    stp x22, x23, [sp, #-16]!
+    bl faktoriyel
+    ldp x22, x23, [sp], #16
+    mov x19, x0
+    adr x0, _fmt_sayi
     mov x1, x19
-    mov x2, x20
     bl printf
     mov x0, #0
     ldp x29, x30, [sp], #16
     ret
+
+kare:
+    stp x29, x30, [sp, #-32]!
+    mov x29, sp
+    str x0, [x29, #-8]
+    ldr x0, [x29, #-8]
+    mov x22, x0
+    ldr x0, [x29, #-8]
+    mov x23, x0
+    mov x0, x22
+    mul x0, x0, x23
+    ldp x29, x30, [sp], #32
+    ret
+    ldp x29, x30, [sp], #32
+    ret
+
+faktoriyel:
+    stp x29, x30, [sp, #-32]!
+    mov x29, sp
+    str x0, [x29, #-8]
+    ldr x0, [x29, #-8]
+    mov x22, x0
+    mov x0, #1
+    cmp x22, x0
+    b.gt _L1
+    mov x0, #1
+    ldp x29, x30, [sp], #32
+    ret
+    b _L2
+_L1:
+_L2:
+    ldr x0, [x29, #-8]
+    mov x22, x0
+    ldr x0, [x29, #-8]
+    mov x22, x0
+    mov x0, #1
+    mov x23, x0
+    mov x0, x22
+    sub x0, x0, x23
+    mov x19, x0
+    mov x0, x19
+    stp x22, x23, [sp, #-16]!
+    bl faktoriyel
+    ldp x22, x23, [sp], #16
+    mov x23, x0
+    mov x0, x22
+    mul x0, x0, x23
+    ldp x29, x30, [sp], #32
+    ret
+    ldp x29, x30, [sp], #32
+    ret
 .section .data
-_var_isim: .quad 0
-_str_1: .asciz "Zedin"
-_var_versiyon: .quad 0
-_str_2: .asciz "Dil: "
-_str_3: .asciz "Versiyon: "
 _fmt_sayi: .byte 37, 100, 10, 0
 _fmt_str_sayi: .byte 37, 115, 37, 100, 10, 0
 _fmt_str_str: .byte 37, 115, 37, 115, 10, 0
