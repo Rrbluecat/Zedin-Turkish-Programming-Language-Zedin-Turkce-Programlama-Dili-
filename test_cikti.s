c248ede2d2ff1171ca3f4c92de33c12a270aad21
Rrbluecat <pelitabdulkadir58@tutamail.com>
2026-08-06 16:15:48 +0300

    Faz 3 %100: zorunlu hata ele alma uyarisi + Faz 4 native compilation tamamlandi

--- test_cikti.s
+++ test_cikti.s
@@ -3,33 +3,20 @@
 main:
     stp x29, x30, [sp, #-16]!
     mov x29, sp
-    mov x0, #3
-    mov x19, x0
-    mov x0, #4
-    mov x20, x0
-    mov x0, x19
-    mov x1, x20
-    stp x22, x23, [sp, #-16]!
-    bl topla
-    ldp x22, x23, [sp], #16
-    mov x22, x0
-    mov x0, #2
-    mov x23, x0
-    mov x0, x22
-    mul x0, x0, x23
-    mov x19, x0
-    adr x0, _fmt_sayi
-    mov x1, x19
-    bl printf
     mov x0, #10
-    mov x19, x0
-    mov x0, #5
-    mov x20, x0
-    mov x0, x19
-    mov x1, x20
-    stp x22, x23, [sp, #-16]!
-    bl topla
-    ldp x22, x23, [sp], #16
+    adr x1, _str_1_0
+    str x0, [x1]
+    mov x0, #20
+    adr x1, _str_1_1
+    str x0, [x1]
+    mov x0, #30
+    adr x1, _str_1_2
+    str x0, [x1]
+    adr x0, _str_1_0
+    adr x1, _var_liste
+    str x0, [x1]
+    adr x1, _var_liste
+    ldr x0, [x1]
     mov x19, x0
     adr x0, _fmt_sayi
     mov x1, x19
@@ -37,23 +24,12 @@ main:
     mov x0, #0
     ldp x29, x30, [sp], #16
     ret
-
-topla:
-    stp x29, x30, [sp, #-32]!
-    mov x29, sp
-    str x0, [x29, #-8]
-    str x1, [x29, #-16]
-    ldr x0, [x29, #-8]
-    mov x22, x0
-    ldr x0, [x29, #-16]
-    mov x23, x0
-    mov x0, x22
-    add x0, x0, x23
-    ldp x29, x30, [sp], #32
-    ret
-    ldp x29, x30, [sp], #32
-    ret
 .section .data
+_var_liste: .quad 0
+_str_1_0: .quad 0
+_str_1_1: .quad 0
+_str_1_2: .quad 0
+_str_1_len: .quad 3
 _fmt_sayi: .byte 37, 100, 10, 0
 _fmt_str_sayi: .byte 37, 115, 37, 100, 10, 0
 _fmt_str_str: .byte 37, 115, 37, 115, 10, 0
